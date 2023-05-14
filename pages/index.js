e4f8e01117e0043c67f9bb5633c14ab1a845c8f1
carlocortez <64764789+Cotrii@users.noreply.github.com>
2023-05-14 16:17:05 +0800

    Added a prototype link (test)

--- pages/index.js
+++ pages/index.js
@@ -125,7 +125,11 @@ export default function Home() {
             </p>
           </div>
           <div className='flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap'>
-            <div className='basis-1/3 flex-1'><Image src={proj1} className="rounded-lg object-cover" width={'100%'} height={'100%'} layout="responsive"/></div>
+            <div className='basis-1/3 flex-1'>
+              <Link href="https://www.figma.com/proto/xsNDoSxHr8EwGZZghOQAtb/Untitled?node-id=1-2&starting-point-node-id=6%3A264">
+              <Image src={proj1} className="rounded-lg object-cover" width={'100%'} height={'100%'} layout="responsive"/>
+              </Link>
+            </div>
             <div className='basis-1/3 flex-1'><Image src={proj2} className="rounded-lg object-cover" width={'100%'} height={'100%'} layout="responsive"/></div>
             <div className='basis-1/3 flex-1'><Image src={proj3} className="rounded-lg object-cover" width={'100%'} height={'100%'} layout="responsive"/></div>
             <div className='basis-1/3 flex-1'><Image src={proj4} className="rounded-lg object-cover" width={'100%'} height={'100%'} layout="responsive"/></div>
